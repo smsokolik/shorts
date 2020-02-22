1dd3cc2604a42b61ef71263edc7751d2192766e9
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-02-22 15:54:26 -0600

    added signup component and troubleshooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'user', component: UserComponent},
+  {path: 'signup', component: SignupComponent},
   {path: '**', redirectTo: '/login'}
   
 ];
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
+  selector: 'app-login',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SigninComponent implements OnInit {
+  loginForm: FormGroup
+  hide: boolean = true;
+  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  signin(e){
+    e.preventDefault();
+    if(this.loginForm.valid){
+      this.userService.signin(this.signinForm.value.username, this.loginForm.value.password);
+    }
   }
-
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(3)])],
+      password: ['', Validators.compose([Validators.required, Validators.maxLength (128), Validators.minLength(8)])]
+    });
+  }
+  
 }
