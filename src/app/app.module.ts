cbd73bfa3c5988bab7f80f2bd36fbe3ad6cb11dd
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-02-22 11:26:06 -0600

    created a user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MaterialModule} from './material-module';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { 
 
-private newMethod() {
-  return material - module;
-}
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup
+  hide: boolean = true;
+  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login(e){
+    e.preventDefault();
+    if(this.loginForm.valid){
+      this.userService.login(this.loginForm.value.username, this.loginForm.value.password);
+    }
   }
-
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(3)])],
+      password: ['', Validators.compose([Validators.required, Validators.maxLength (128), Validators.minLength(8)])]
+    });
+  }
+  
 }
