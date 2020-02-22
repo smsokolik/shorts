31ab00cb80e3212be724f6708e08adbfcdebde95
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-02-22 11:51:30 -0600

    adding background color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'user', component: UserComponent},
   {path: '**', redirectTo: '/login'}
   
 ];
