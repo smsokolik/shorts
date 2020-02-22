b3fb3b7de0055bfb00d03414473c787525d488c4
Shannon Sokolik <shannonsokolik@Shannons-MacBook-Air.local>
2020-02-22 09:50:52 -0600

    added angular material and forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MaterialModule} from './material-module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,16 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+private newMethod() {
+  return material - module;
+}
+}
